48d4e87b558fd323ee0c167512b10c7dd4a07445
gokay <gokay@GOKAY>
2024-12-24 19:29:45 +0300

    Add project files.

---
+++ practıc while do-while/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Bir limit değeri girin: ");
+        int limit = int.Parse(Console.ReadLine());
+
+        int sayac = 0;
+
+        while (sayac <= limit)
+        {
+            Console.WriteLine("Ben bir Patika'lıyım");
+            sayac++;  
+        }
+    }
+}
+
+
+// do while da kontrol sonda oldugu için en az 1 kere yazıyor fakat whileda basta oldugu icin olumsuz olabiliyor
